99c2a8ad7bd7bee29167d834e115de35ef0cdefb
guifroes1984 <guilhermefroes26@gmail.com>
2025-02-15 18:33:40 -0300

    Contrução da Classe SecurityFilter.

--- src/main/java/br/com/guifroes/agendamento/repositories/UsuarioRepository.java
+++ src/main/java/br/com/guifroes/agendamento/repositories/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.guifroes.agendamento.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.guifroes.agendamento.domain.usuario.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+	
+	Optional<Usuario> findByEmail(String email);
 
 }
